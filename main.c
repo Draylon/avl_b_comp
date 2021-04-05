64c75f1f607764358ff1f13d22f9ddb9f3fdab40
Draylon Vieira <draylid@gmail.com>
2021-04-05 17:37:43 -0300

    update

--- main.c
+++ main.c
@@ -4,23 +4,41 @@
 //#include "avl.h"
 #include "b.h"
 
+int* random_array(const int size){
+    int rand_array[size];
+    int ii=0;
+    while(ii < size){
+        int r_int = rand();
+
+        int match=0; // BOOL
+        for(int iic=0;iic < ii;iic++){
+            if(rand_array[iic] == r_int)
+                match=1;
+                break;
+        }
+        if(match==0){
+            rand_array[ii] = r_int;
+            ii++;
+        }else
+            continue;
+    }
+
+    for(int iitt=0;iitt<5;iitt++){
+        for(int iit=0;iit < 8;iit++){
+            printf("%d ",rand_array[iit+8*iitt]);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
     srand(time(NULL));
     
 
-    int rand_array[40];
-    int ii=0;
-    while(ii < 40){
-        int r_int = rand();
-        int match=1; // BOOL
-        for(int iic=0;iic < ii;iic++){
-            if(rand_array[iic] == r_int)
-                break;
-        }
-    }
+    int* rand_array = random_array(40);
 
     
-    ArvoreB* arvore = criaArvore(1);
+    /*ArvoreB* arvore = criaArvore(1);
     adicionaChave(arvore, 12);
     adicionaChave(arvore, 3);
     adicionaChave(arvore, 5);
@@ -32,5 +50,5 @@ int main(){
 
     percorreArvore(arvore->raiz);
 
-    printf("\nNumero de operacoes %d\n", contadorB);
+    printf("\nNumero de operacoes %d\n", contadorB);*/
 }
